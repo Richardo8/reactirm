d31e32ff20760cd53dbccb4e2d423b3f23e4c064
LS <manunited_22@sina.com>
2017-08-07 16:00:10 +0800

    添加reducers

--- src/Redux/Reducers/userReducers.js
+++ src/Redux/Reducers/userReducers.js
@@ -1,6 +1,28 @@
 import { combineReducers } from 'redux'
-import { GET_USERS_REQUEST, GET_USERS_RECEIVE} from "../Action/userAction"
+import { GET_USERS_RECEIVE} from "../Action/userAction"
 
-function getUsers(state = {}, action) {
-
+function posts(state = {}, action) {
+    switch (action.type) {
+        case GET_USERS_RECEIVE:
+            return Object.assign({}, state, {
+                items: action.posts
+            })
+        default:
+            return state
+    }
 }
+
+function postsBySubreddit(state = { }, action) {
+    switch (action.type) {
+        case GET_USERS_RECEIVE:
+            return Object.assign({}, state, {
+                [action.subreddit]: posts(state[action.subreddit], action)
+            })
+        default:
+            return state
+    }
+}
+
+export default rootReducers = combineReducers({
+    postsBySubreddit
+})
