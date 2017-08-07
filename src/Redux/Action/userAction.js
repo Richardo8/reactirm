aeae271d3c7631d84c61291162145f16dfcfab5d
LS <manunited_22@sina.com>
2017-08-07 15:45:20 +0800

    暂时用不到requestPosts

--- src/Redux/Action/userAction.js
+++ src/Redux/Action/userAction.js
@@ -3,12 +3,12 @@ export const GET_USERS_RECEIVE = 'GET_ALL_USERS_RECEIVE'
 // export const GET_SEARCH_USERS_REQUEST = 'GET_SEARCH_USERS_REQUEST'
 // export const GET_SEARCH_USERS_RECEIVE = 'GET_SEARCH_USERS_RECEIVE'
 
-function requestPosts(subreddit) {
-    return {
-        type: GET_USERS_REQUEST,
-        subreddit
-    }
-}
+// function requestPosts(subreddit) {
+//     return {
+//         type: GET_USERS_REQUEST,
+//         subreddit
+//     }
+// }
 
 function receivePosts(subreddit, json) {
     return {
@@ -20,7 +20,6 @@ function receivePosts(subreddit, json) {
 
 export function fetchPosts(subreddit) {
     return async dispatch => {
-        dispatch(requestPosts(subreddit))
         let res = await fetch(`/userlist`)
         let resJson = res.json()
         return dispatch(receivePosts(subreddit, resJson))
---
+++ src/Redux/Reducers/userReducers.js
@@ -0,0 +1,6 @@
+import { combineReducers } from 'redux'
+import { GET_USERS_REQUEST, GET_USERS_RECEIVE} from "../Action/userAction"
+
+function getUsers(state = {}, action) {
+
+}
