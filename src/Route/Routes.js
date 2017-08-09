67d2bddad35ecbd92364a07c752943879672694c
LS <manunited_22@sina.com>
2017-08-09 11:51:32 +0800

    exact参数用来控制是否严格遵守路由路径的定义，如果为true，就不会发生跳转/article同时渲染两个组件的情况

--- src/Route/Routes.js
+++ src/Route/Routes.js
@@ -3,16 +3,58 @@ import { Route } from 'react-router-dom'
 import Index from "../components/Index/Index";
 import UserListPage from "../components/Page/UserListPage";
 import GroupListPage from "../components/Page/GroupListPage";
+import PublishComponent from "../components/Content/Article/Publish";
+import ArticlePage from "../components/Page/ArticlePage";
 
+const routes = [
+    {
+        path: '/',
+        component: Index,
+        exact: true
+    },
+    { path: '/userlist',
+        component: UserListPage
+    },
+    { path: '/search',
+        component: UserListPage
+    },
+    { path: '/grouplist',
+        component: GroupListPage
+    },
+    { path: '/article',
+        component: ArticlePage,
+        routes: [
+            { path: '/article/publish',
+                component: PublishComponent
+            },
+        ]
+    }
+]
+
+const RouteWithSubRoutes = (route) => (
+    <Route path={route.path} exact={route.exact} render={props => (
+        // pass the sub-routes down to keep nesting
+        <route.component {...props} routes={route.routes}/>
+    )}/>
+)
 
 export default class Routes extends React.Component {
     render(){
+        console.log(routes.map((route, i) => (
+            <RouteWithSubRoutes key={i} {...route}/>
+        )))
         return (
                 <div>
                     <Route exact path="/" component={Index}/>
                     <Route path="/userlist" component={UserListPage}/>
                     <Route path="/search" component={UserListPage}/>
                     <Route path="/grouplist" component={GroupListPage}/>
+                    <Route exact path="/article" component={ArticlePage}>
+                    </Route>
+                    <Route exact path="/article/publish" component={PublishComponent}/>
+                    {/*{routes.map((route, i) => (*/}
+                        {/*<RouteWithSubRoutes key={i} {...route}/>*/}
+                    {/*))}*/}
                 </div>
 
         )
