1655f3f20060b1738069f2cdd2ec01e7a622abc6
LS <manunited_22@sina.com>
2017-08-09 17:15:26 +0800

    添加上传组件

--- src/components/Content/Article/Publish.js
+++ src/components/Content/Article/Publish.js
@@ -1,33 +1,24 @@
 import React, { Component } from 'react'
-import { Form, Input, Tooltip, Icon, Cascader, Select, Row, Col, Checkbox, Button, AutoComplete, Upload, message } from 'antd';
+import { Form, Input, Icon, Cascader, Button, Upload, message } from 'antd';
 import './publish.css'
 const { TextArea } = Input
 const FormItem = Form.Item;
-const Option = Select.Option;
-const AutoCompleteOption = AutoComplete.Option;
 
 const residences = [{
-    value: 'zhejiang',
-    label: 'Zhejiang',
-    children: [{
-        value: 'hangzhou',
-        label: 'Hangzhou',
-        children: [{
-            value: 'xihu',
-            label: 'West Lake',
-        }],
-    }],
+    value: 'a',
+    label: 'a',
 }, {
-    value: 'jiangsu',
-    label: 'Jiangsu',
-    children: [{
-        value: 'nanjing',
-        label: 'Nanjing',
-        children: [{
-            value: 'zhonghuamen',
-            label: 'Zhong Hua Men',
-        }],
-    }],
+    value: 'b',
+    label: 'b',
+}, {
+    value: 'c',
+    label: 'c',
+}, {
+    value: 'd',
+    label: 'd',
+}, {
+    value: 'e',
+    label: 'e',
 }];
 
 function getBase64(img, callback) {
@@ -66,31 +57,6 @@ class Publish extends Component {
         const value = e.target.value;
         this.setState({ confirmDirty: this.state.confirmDirty || !!value });
     }
-    checkPassword = (rule, value, callback) => {
-        const form = this.props.form;
-        if (value && value !== form.getFieldValue('password')) {
-            callback('Two passwords that you enter is inconsistent!');
-        } else {
-            callback();
-        }
-    }
-    checkConfirm = (rule, value, callback) => {
-        const form = this.props.form;
-        if (value && this.state.confirmDirty) {
-            form.validateFields(['confirm'], { force: true });
-        }
-        callback();
-    }
-
-    handleWebsiteChange = (value) => {
-        let autoCompleteResult;
-        if (!value) {
-            autoCompleteResult = [];
-        } else {
-            autoCompleteResult = ['.com', '.org', '.net'].map(domain => `${value}${domain}`);
-        }
-        this.setState({ autoCompleteResult });
-    }
 
     handleChange = (info) => {
         if (info.file.status === 'done') {
@@ -101,7 +67,6 @@ class Publish extends Component {
 
     render() {
         const { getFieldDecorator } = this.props.form;
-        const { autoCompleteResult } = this.state;
         const imageUrl = this.state.imageUrl;
 
         const formItemLayout = {
@@ -126,18 +91,6 @@ class Publish extends Component {
                 },
             },
         };
-        const prefixSelector = getFieldDecorator('prefix', {
-            initialValue: '86',
-        })(
-            <Select style={{ width: 60 }}>
-                <Option value="86">+86</Option>
-                <Option value="87">+87</Option>
-            </Select>
-        );
-
-        const websiteOptions = autoCompleteResult.map(website => (
-            <AutoCompleteOption key={website}>{website}</AutoCompleteOption>
-        ));
 
         return (
             <Form className="FormMaxWidth" onSubmit={this.handleSubmit}>
@@ -146,11 +99,9 @@ class Publish extends Component {
                     label="文章标题"
                     hasFeedback
                 >
-                    {getFieldDecorator('email', {
+                    {getFieldDecorator('文章标题', {
                         rules: [{
-                            type: 'email', message: 'The input is not valid E-mail!',
-                        }, {
-                            required: true, message: 'Please input your E-mail!',
+                            required: true, message: '请输入文章标题!',
                         }],
                     })(
                         <Input />
@@ -160,9 +111,9 @@ class Publish extends Component {
                     {...formItemLayout}
                     label="频道栏目"
                 >
-                    {getFieldDecorator('residence', {
-                        initialValue: ['zhejiang', 'hangzhou', 'xihu'],
-                        rules: [{ type: 'array', required: true, message: 'Please select your habitual residence!' }],
+                    {getFieldDecorator('频道栏目', {
+                        initialValue: ['a'],
+                        rules: [{ type: 'array', required: true, message: '请选择频道栏目！' }],
                     })(
                         <Cascader options={residences} />
                     )}
@@ -172,14 +123,12 @@ class Publish extends Component {
                     label="作者来源"
                     hasFeedback
                 >
-                    {getFieldDecorator('confirm', {
+                    {getFieldDecorator('作者来源', {
                         rules: [{
-                            required: true, message: 'Please confirm your password!',
-                        }, {
-                            validator: this.checkPassword,
+                            required: true, message: '请输入作者来源!',
                         }],
                     })(
-                        <Input type="password" onBlur={this.handleConfirmBlur} />
+                        <Input type="text"/>
                     )}
                 </FormItem>
                 <FormItem
@@ -187,11 +136,9 @@ class Publish extends Component {
                     label="封面图上传"
                     hasFeedback
                 >
-                    {getFieldDecorator('confirm', {
+                    {getFieldDecorator('封面图上传', {
                         rules: [{
-                            required: true, message: 'Please confirm your password!',
-                        }, {
-                            validator: this.checkPassword,
+                            required: true, message: '请上传封面图!',
                         }],
                     })(
                         <Upload
@@ -215,98 +162,16 @@ class Publish extends Component {
                     label="摘要"
                     hasFeedback
                 >
-                    {getFieldDecorator('confirm', {
+                    {getFieldDecorator('摘要', {
                         rules: [{
-                            required: true, message: 'Please confirm your password!',
-                        }, {
-                            validator: this.checkPassword,
+                            required: true, message: '请输入摘要!',
                         }],
                     })(
                         <TextArea rows={4} onBlur={this.handleConfirmBlur} />
                     )}
                 </FormItem>
-                <FormItem
-                    {...formItemLayout}
-                    label={(
-                        <span>
-              Nickname&nbsp;
-                            <Tooltip title="What do you want other to call you?">
-                <Icon type="question-circle-o" />
-              </Tooltip>
-            </span>
-                    )}
-                    hasFeedback
-                >
-                    {getFieldDecorator('nickname', {
-                        rules: [{ required: true, message: 'Please input your nickname!', whitespace: true }],
-                    })(
-                        <Input />
-                    )}
-                </FormItem>
-                <FormItem
-                    {...formItemLayout}
-                    label="Habitual Residence"
-                >
-                    {getFieldDecorator('residence', {
-                        initialValue: ['zhejiang', 'hangzhou', 'xihu'],
-                        rules: [{ type: 'array', required: true, message: 'Please select your habitual residence!' }],
-                    })(
-                        <Cascader options={residences} />
-                    )}
-                </FormItem>
-                <FormItem
-                    {...formItemLayout}
-                    label="Phone Number"
-                >
-                    {getFieldDecorator('phone', {
-                        rules: [{ required: true, message: 'Please input your phone number!' }],
-                    })(
-                        <Input addonBefore={prefixSelector} style={{ width: '100%' }} />
-                    )}
-                </FormItem>
-                <FormItem
-                    {...formItemLayout}
-                    label="Website"
-                >
-                    {getFieldDecorator('website', {
-                        rules: [{ required: true, message: 'Please input website!' }],
-                    })(
-                        <AutoComplete
-                            dataSource={websiteOptions}
-                            onChange={this.handleWebsiteChange}
-                            placeholder="website"
-                        >
-                            <Input />
-                        </AutoComplete>
-                    )}
-                </FormItem>
-                <FormItem
-                    {...formItemLayout}
-                    label="Captcha"
-                    extra="We must make sure that your are a human."
-                >
-                    <Row gutter={8}>
-                        <Col span={12}>
-                            {getFieldDecorator('captcha', {
-                                rules: [{ required: true, message: 'Please input the captcha you got!' }],
-                            })(
-                                <Input size="large" />
-                            )}
-                        </Col>
-                        <Col span={12}>
-                            <Button size="large">Get captcha</Button>
-                        </Col>
-                    </Row>
-                </FormItem>
-                <FormItem {...tailFormItemLayout} style={{ marginBottom: 8 }}>
-                    {getFieldDecorator('agreement', {
-                        valuePropName: 'checked',
-                    })(
-                        <Checkbox>I have read the <a href="">agreement</a></Checkbox>
-                    )}
-                </FormItem>
                 <FormItem {...tailFormItemLayout}>
-                    <Button type="primary" htmlType="submit">Register</Button>
+                    <Button type="primary" htmlType="submit">发布</Button>
                 </FormItem>
             </Form>
         );
