7f80b9bf719a784c0827b36f14214400d665c181
LS <manunited_22@sina.com>
2017-08-10 11:59:10 +0800

    添加秀米编辑器

--- src/components/Editor/Ueditor.js
+++ src/components/Editor/Ueditor.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
+import './xiumi-ue-v5.css'
 const {UE} = window
 
 class Ueditor extends Component {
     state = {}
 
     componentDidMount(){
+        const baseUrl = `/JS/plugins/ueditor1_4_3/dialogs/xiumi/`
+        this.initXiuMi(baseUrl)
         this.initEditor()
     }
 
@@ -30,6 +33,27 @@ class Ueditor extends Component {
                 'font-family' : "'微软雅黑'", 'font-size' : '14px', 'line-height' : '1.5', 'margin' : '5px 100px 5px 100px'
             });
         })
+    }
+
+    initXiuMi(baseURL) {
+        UE.registerUI('dialog', (editor, uiName) => {
+            return new UE.ui.Button({
+                name: 'xiumi-connect',
+                title: '秀米',
+                onclick() {
+                    const dialog = new UE.ui.Dialog({
+                        iframeUrl: `${baseURL}xiumi-ue-dialog-v5.html`,
+                        editor,
+                        name: 'xiumi-connect',
+                        title: '秀米图文消息助手',
+                        cssRules: `width: ${window.innerWidth - 60}px;` + `height: ${window.innerHeight - 60}px;`,
+                    });
+
+                    dialog.render();
+                    dialog.open();
+                },
+            });
+        });
     }
 
     render(){
